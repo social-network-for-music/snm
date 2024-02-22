2da600468ca324cb43979f6f59eacdc4d3a63d7d
Davide Casale <davidecasale2002@gmail.com>
2024-02-22 04:55:08 +0100

    Change responsiveness of @/components/message.tsx.

--- src/components/message.tsx
+++ src/components/message.tsx
@@ -9,12 +9,12 @@ export interface IMessageProps extends PropsWithChildren {
 export default function Message(props: IMessageProps) {
     return (
         <div className={`${props.className} w-full h-full text-wrap text-center`}>
-            <div className="text-white xs:text-lg sm:text-xl font-bold">
+            <div className="text-white xs:text-sm sm:text-xl font-bold">
                 { props.children }
             </div>
 
             { props.description && (
-                <div className="text-[#C1C1C1] text-md font-semibold mt-2">
+                <div className="text-[#C1C1C1] xs:text-xs sm:text-sm font-semibold xs:mt-1 sm:mt-2">
                     { props.description }
                 </div>
             )}
