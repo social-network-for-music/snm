ac38709b5ca440b7b794475e4622922d70e85062
Davide Casale <davidecasale2002@gmail.com>
2024-02-28 20:56:16 +0100

    Add interface for HTTP endpoints: PATCH /api/users, PATCH /api/users/password and DELETE /api/users.

--- src/api/users.api.ts
+++ src/api/users.api.ts
@@ -26,6 +26,54 @@ export function post(data: IPostData): Promise<AxiosResponse<IUser>> {
     );
 }
 
+export interface IPatchData {
+    username?: string;
+    artists?: string[];
+    genres?: string[];
+}
+
+export function patch(data: IPatchData): Promise<AxiosResponse<IUser>> {
+    const token = localStorage.getItem("token");
+
+    const headers = {
+        "Authorization": `Bearer ${token}`,
+
+        "Content-Type": "application/json"
+    };
+
+    return axios(`${process.env.API}/users`,
+        {
+            method: "PATCH",
+            headers: headers,
+            data: data
+        }
+    );
+}
+
+export interface IPasswordData {
+    password: string;
+
+    oldPassword: string;
+}
+
+export function password(data: IPasswordData): Promise<AxiosResponse<IUser>> {
+    const token = localStorage.getItem("token");
+
+    const headers = {
+        "Authorization": `Bearer ${token}`,
+
+        "Content-Type": "application/json"
+    };
+
+    return axios(`${process.env.API}/users/password`,
+        {
+            method: "PATCH",
+            headers: headers,
+            data: data
+        }
+    );
+}
+
 export function get(): Promise<AxiosResponse<IUser>> {
     const token = localStorage.getItem("token");
 
@@ -34,6 +82,22 @@ export function get(): Promise<AxiosResponse<IUser>> {
             method: "GET",
 
             headers: { "Authorization": `Bearer ${token}` }
+        }
+    );
+}
+
+export function del(): Promise<AxiosResponse<void>> {
+    const token = localStorage.getItem("token");
+
+    const headers = {
+        "Authorization": `Bearer ${token}`
+    };
+
+    return axios(`${process.env.API}/users`,
+        {
+            method: "DELETE",
+
+            headers: headers
         }
     );
 }
