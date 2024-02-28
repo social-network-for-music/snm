c5ca5ba848d3e9f09b3a6c9ad84c9424dc26f719
Davide Casale <davidecasale2002@gmail.com>
2024-02-28 19:20:54 +0100

    Fix strange bug in @/components/playlists/utilities/thumbnail.tsx.

--- src/components/playlists/utilities/thumbnail.tsx
+++ src/components/playlists/utilities/thumbnail.tsx
@@ -8,49 +8,11 @@ import * as _playlists from "@/api/playlists.api";
 
 const PLACEHOLDER = "/static/playlist-placeholder.webp";
 
-interface IContainerProps {
-    rows: number;
-    columns: number;
-    images: string[];
-
-    gradient?: boolean;
-
-    className?: string;
-}
-
-function Container(props: IContainerProps) {
-    const { 
-        rows,
-        columns,
-        className
-    } = props;
-
-    function padEnd<T = any>(array: T[], length: number, value?: T): T[] {
-        return Object.assign(new Array(length).fill(value), array);
-    }
-
-    const images = padEnd(props.images, rows * columns, PLACEHOLDER);
-
-    return (
-        <div 
-            className={`${className} grid grid-rows-${rows} grid-cols-${columns} overflow-hidden relative`}
-        >
-            { images.map((image, i) => (
-                <img 
-                    key={i}
-                    src={image}
-                    className="w-full h-full object-cover"
-                />
-            ))}
-
-            { props.gradient &&
-                <div className="absolute w-full h-full bg-gradient-to-b from-transparent to-spotify-darkgray">
-                    
-                </div>
-            }
-        </div>
-    );
-}
+const Gradient = (
+    <div className="absolute w-full h-full bg-gradient-to-b from-transparent to-spotify-darkgray">
+        {/* Nothing to do. */}                 
+    </div>
+);
 
 export interface IThumbnailProps {
     id: string;
@@ -70,14 +32,42 @@ export default function Thumbnail(props: IThumbnailProps) {
     }, [props.id]);
 
     return (
-        <Container 
-            rows={thumbnails.length <= 2 ? 1 : 2}
-            columns={thumbnails.length <= 1 ? 1 : 2}
-            images={thumbnails}
+        <>
+            { thumbnails.length <= 1 &&
+                <div className={`${props.className} grid grid-rows-1 grid-cols-1 overflow-hidden relative`}>
+                    <img src={thumbnails[0] ?? PLACEHOLDER} className="w-full h-full object-cover" />
 
-            gradient={props.gradient}
+                    { props.gradient &&
+                        Gradient
+                    }
+                </div>
+            }
 
-            className={props.className}
-        />
+            { thumbnails.length == 2 &&
+                <div className={`${props.className} grid grid-rows-1 grid-cols-2 overflow-hidden relative`}>
+                    <img src={thumbnails[0]} className="w-full h-full object-cover" />
+
+                    <img src={thumbnails[1]} className="w-full h-full object-cover" />
+
+                    { props.gradient &&
+                        Gradient
+                    }
+                </div>
+            }
+
+            { thumbnails.length >= 3 &&
+                <div className={`${props.className} grid grid-rows-2 grid-cols-2 overflow-hidden relative`}>
+                    <img src={thumbnails[0]} className="w-full h-full object-cover"/>
+                    <img src={thumbnails[1]} className="w-full h-full object-cover"/>
+                    <img src={thumbnails[2]} className="w-full h-full object-cover"/>
+
+                    <img src={thumbnails[3] ?? PLACEHOLDER} className="w-full h-full object-cover"/>
+
+                    { props.gradient &&
+                        Gradient
+                    }
+                </div>
+            }
+        </>
     );
 }
